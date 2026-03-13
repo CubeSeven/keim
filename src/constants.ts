e4ffcab86cee921aa371f55c83f0376ecbea6833
kapsalaspanos <kapsalaspanos@gmail.com>
2026-03-13 14:26:27 +0200

    Updates

--- src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const APP_VERSION = '1.7.0';
-export const ENABLE_SMART_PROPS = true;
--- test-html.js
+++
@@ -1,2 +0,0 @@
-const { Plugin, PluginKey } = require('@milkdown/kit/prose/state');
-console.log("Success");
